85a098dcebf1053091b94ca47e705779854bb1a5
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2026-06-28 11:08:44 -0600

    Fix ruler bug that used overland speed even if another speed is selected if overland is faster

--- src/module/canvas/ruler.js
+++ src/module/canvas/ruler.js
@@ -68,18 +68,20 @@ export default class TokenRulerPTU extends foundry.canvas.placeables.tokens.Toke
 
   #getSpeedBasedStyle(waypoint, style) {
     try {
-      if (!(game.user.id in this.token._plannedMovement) || (CONFIG.Token?.movement?.actions?.[waypoint.action]?.teleport)) return style;
+      if (style?.alpha === 0) return style;
+      if (!(this.token._plannedMovement && (game.user.id in this.token._plannedMovement)) || (CONFIG.Token?.movement?.actions?.[waypoint.action]?.teleport)) return style;
 
       const movement = this.token.actor?.system?.capabilities;
       if (!movement) return style;
 
       const movementType = (()=>{
         const allOptions = Object.entries(CONFIG.PTU.tokenMovementCapabilityMap).filter(([key, value]) => (value === waypoint.action));
+        if (!allOptions.length) return "overland";
         // find the fastest movement type that can perform the current action
-        return allOptions.reduce((fastest, [key, value]) => {
+        return allOptions.reduce((fastest, [key]) => {
           if ((movement[key] ?? 0) > (movement[fastest] ?? 0)) return key;
           return fastest;
-        }, "overland");
+        }, allOptions[0][0]);
       })();
 
       // Determine the current action speed. Fall back to "overland" when missing.
